f9cbd35449cccfb04012f646e525395b84b99920
jeongyeon <jeongyeon.park216@gmail.com>
2024-04-29 13:11:36 +0900

    [Fix] Added UUID option
    Former-commit-id: 18be4a028198c9cdfca259c9f6f619061e72dfff

--- moeum-back/moeum/src/main/java/com/b301/moeum/user/entity/User.java
+++ moeum-back/moeum/src/main/java/com/b301/moeum/user/entity/User.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4f8aba9875d5896ecc5e409b9922d2d4e1693d4b5571aa5920ff782cdf1bb4fd
-size 2171
+oid sha256:be2b03f9bab66dfa9701821b606d4811ee75940b201d974ce0a61294f6d486dc
+size 2190
--- moeum-back/moeum/src/main/java/com/b301/moeum/user/service/UserService.java
+++ moeum-back/moeum/src/main/java/com/b301/moeum/user/service/UserService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d050d12a2ff63cc8e77d95bfd3868f69dde43089fe0d50e64e1e2a32e77a14ce
-size 7225
+oid sha256:0dba38ff9f5f4a64e2b28b114faffa65497072b125f29097bd158069f1e1c92e
+size 7272
