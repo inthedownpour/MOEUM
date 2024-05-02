b9959ab3c2f11f2cda746a02de1e2627d68b98fc
jeongyeon <jeongyeon.park216@gmail.com>
2024-05-02 10:32:33 +0900

    [Fix] Change response object
    Former-commit-id: 821701543f61c82ed92f5eba2f583270caf8983a

--- moeum-back/moeum/src/main/java/com/b301/moeum/user/controller/UserController.java
+++ moeum-back/moeum/src/main/java/com/b301/moeum/user/controller/UserController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:25e1bfba6126fe16dd4ce70c0187575e067dba19d830ce83a3f151eebcd84f25
-size 5128
+oid sha256:6be5d52935c64dcdaf58988b52c610d088f4ff64330cc3fb36ea4371cacca0aa
+size 4940
--- moeum-back/moeum/src/main/java/com/b301/moeum/user/service/UserService.java
+++ moeum-back/moeum/src/main/java/com/b301/moeum/user/service/UserService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:85c9caa7b23dd8331d7efd174391ef52971210ff4b654694f7b14e2d7c530e29
-size 7958
+oid sha256:9d1be2d3a802d73a0d84781f408bcc2e848168a948818004d54e04bd702b8591
+size 8062
