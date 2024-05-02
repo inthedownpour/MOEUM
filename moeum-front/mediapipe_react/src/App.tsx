b542fd571283061dd941f57f104da29f4c771605
주동현 <wn196785@gmail.com>
2024-05-03 03:00:10 +0900

    [feat] MVP alpha.ver
    Former-commit-id: 79a5f4c0142a856a62ae7c4ec7cd8096fec83178

--- moeum-front/mediapipe_react/src/App.tsx
+++ moeum-front/mediapipe_react/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e3d3da30e9fcc36b0e1cf34cbb98e03a988062b77caf7a362865b5146d232e1a
-size 4735
+oid sha256:fe35a43c10161ec11a196c32d9692adc16d25e6bb769f7d50dcd4bf89f3a3160
+size 5984
---
+++ moeum-front/mediapipe_react/src/Audio.d.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6142889093f303064d13299808896cc1e0875e64889f72bda210d485d15f0e6a
+size 77
