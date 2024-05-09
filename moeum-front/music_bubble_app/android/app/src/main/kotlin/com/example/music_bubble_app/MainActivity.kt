6a172f33ce4311e6e8cac332fb38703532f90d2a
gabin <3to@kakao.com>
2024-05-09 17:29:29 +0900

    Feat: Music Exception
    Former-commit-id: 2d2446871e4ddf8c2f045b58bffb44ba6b27a665

--- moeum-front/music_bubble_app/android/app/src/main/kotlin/com/example/music_bubble_app/MainActivity.kt
+++ moeum-front/music_bubble_app/android/app/src/main/kotlin/com/example/music_bubble_app/MainActivity.kt
@@ -1,5 +1,3 @@
-package com.example.music_bubble_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
+version https://git-lfs.github.com/spec/v1
+oid sha256:94656c261447be21e6b0b573c8f42b6bd49f08ae8fa043aed56cd235a103edad
+size 129
