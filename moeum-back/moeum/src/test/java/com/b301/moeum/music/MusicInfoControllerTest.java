42f47cbc76c534ddbc7d02b11ea54b650825d61e
gabin <3to@kakao.com>
2024-05-09 14:59:22 +0900

    Test: Edit Music API Test
    Former-commit-id: f5a92639e62c7cc8ca64fdaf049a336bf441d4d5

--- moeum-back/moeum/src/test/java/com/b301/moeum/music/MusicInfoControllerTest.java
+++ moeum-back/moeum/src/test/java/com/b301/moeum/music/MusicInfoControllerTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:311ef466cc32cb90d57fd152d1061c177dd73cd1bbcbbb1ed5f7a66890b2cd65
-size 12871
+oid sha256:7bd8c6c57dc471ddcf100718c340697033bcb1d290cedea7f67af0665e4279ce
+size 15011
--- moeum-back/moeum/src/test/java/com/b301/moeum/music/MusicInfoServiceTest.java
+++ moeum-back/moeum/src/test/java/com/b301/moeum/music/MusicInfoServiceTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:704e519f2ad93beff231c22d10194516a3e46840ee8855bf543944a1c674f9a9
-size 5700
+oid sha256:5ec7069e88d569942ec1ca98f7aa0f741252c5a168ed6b149004bde1016ffcd6
+size 7177
