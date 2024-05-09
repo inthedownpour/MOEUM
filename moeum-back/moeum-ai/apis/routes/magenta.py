f1e16d7561321a5308afa0dab615fd2e3b1e9355
kanggeonnim <gun4329@gmail.com>
2024-05-09 11:21:01 +0900

    Refactor S3 upload logic to separate MIDI and MP3 files, update key generation and upload paths
    Former-commit-id: 2bb1d1bdf8fb29d6baca47addacab135843de76c

--- moeum-back/moeum-ai/apis/routes/magenta.py
+++ moeum-back/moeum-ai/apis/routes/magenta.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1dbe93951c1c1d19c30fbe33e02b39af7a51b73eb4fc38ff0c47244c57fcc185
-size 704
+oid sha256:ef6ff84d4f45be1ad9945e736e479409256c85fb57de0187037602659d7449ea
+size 1213
