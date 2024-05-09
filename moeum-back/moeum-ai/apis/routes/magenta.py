63f138c456cab66eb0d42708672796df294ab9c4
kanggeonnim <gun4329@gmail.com>
2024-05-09 14:48:12 +0900

    Update music generation function to include instrument and bpm parameters.
    Former-commit-id: 7fe87429811b057672c17bdcc66d24affadddbeb

--- moeum-back/moeum-ai/apis/routes/magenta.py
+++ moeum-back/moeum-ai/apis/routes/magenta.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ef6ff84d4f45be1ad9945e736e479409256c85fb57de0187037602659d7449ea
-size 1213
+oid sha256:21ded2eb99e635fed568219bafd4112f49f38f146fba2dd63f26b8f5dad8eb13
+size 1357
