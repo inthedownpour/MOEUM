779dc252b46f42ababa6c04c97ef2bb8a419216a
kanggeonnim <gun4329@gmail.com>
2024-05-09 12:47:30 +0900

    Add functionality to create an MP3 folder if it doesn't exist before converting WAV to MP3 in upload_s3.py
    Former-commit-id: 2c0cf1a5073965fee887f78e9201449458dc57e7

--- moeum-back/moeum-ai/s3/upload_s3.py
+++ moeum-back/moeum-ai/s3/upload_s3.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d5801849ea92a0cc6d47623c7ea6d1a3cc9e70bd8e0b85572fe323024535184
-size 3796
+oid sha256:b37ca27b80cef334f289cbbf6f8d559533f5b0dcb9a15a969cfbbbf87baa2aff
+size 3898
