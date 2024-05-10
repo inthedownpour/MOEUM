848af72b886116fc299b2655446b12603020590f
gabin <3to@kakao.com>
2024-05-10 16:28:28 +0900

    Feat: add UserPrincipal
    Former-commit-id: bd743c7eb199cbe16af386736153eb92473bba81

--- moeum-back/moeum/src/main/java/com/b301/moeum/music/controller/MusicInfoController.java
+++ moeum-back/moeum/src/main/java/com/b301/moeum/music/controller/MusicInfoController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3686c64c6e5a14255b18ee31a73b868210be9ebf0c4ce6f4b8a0f20480e871da
-size 2284
+oid sha256:7eab62f443afc51032e9fed984ec923b007f2d4a11b674ff1881cd40887b1667
+size 2392
