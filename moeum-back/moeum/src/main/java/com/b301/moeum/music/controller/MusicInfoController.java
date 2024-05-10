83ab8404e97a4645b84ac9c30d6e1869ea7aa4bb
gabin <3to@kakao.com>
2024-05-10 16:27:36 +0900

    Refactor: Music EndPoint
    Former-commit-id: d1ce4fa2a6e24d692cc60691ef03a9d55218ca9b

--- moeum-back/moeum/src/main/java/com/b301/moeum/music/controller/MusicInfoController.java
+++ moeum-back/moeum/src/main/java/com/b301/moeum/music/controller/MusicInfoController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b236056e534d7c394a7fd066cc2e0045955454d7d575d6628d29e702b85c82e2
-size 2281
+oid sha256:3686c64c6e5a14255b18ee31a73b868210be9ebf0c4ce6f4b8a0f20480e871da
+size 2284
--- moeum-front/mediapipe_react/src/custom.d.ts
+++ moeum-front/mediapipe_react/src/custom.d.ts
@@ -1,4 +1,3 @@
-declare module "*.mp3" {
-    const src: string;
-    export default src;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:96e858289c68c23eef3ee9cbf687ea98181b834e7b13174f66ea88da51e5b13c
+size 73
