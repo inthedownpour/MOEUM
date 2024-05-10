8b8ae2c232352546498eaae1184a346e574a979d
주동현 <wn196785@gmail.com>
2024-05-10 14:38:10 +0900

    [fix] flutter connect error fixed
    Former-commit-id: dc671283725aa0a878dc30bc2fd0cc6cd9249a7a

--- moeum-front/mediapipe_react/src/App.tsx
+++ moeum-front/mediapipe_react/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:322c726bc3e79af8e984a4d45a81074a81455fadac98035df5ff33909fc0c753
-size 16445
+oid sha256:19e9379b331c0fdbca93a174683a5115e3cab96bd29c485c1d3564c758edfd7e
+size 16398
