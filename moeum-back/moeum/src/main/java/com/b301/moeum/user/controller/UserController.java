77ee63794ab513846adb835d4381bd37c6ab7a27
jeongyeon <jeongyeon.park216@gmail.com>
2024-05-11 15:01:02 +0900

    [Fix] Altered ATK & RTK check logic
    Former-commit-id: e1353261ab0ebd2d424518d440ef1def95cb6a43

--- moeum-back/moeum/src/main/java/com/b301/moeum/user/controller/UserController.java
+++ moeum-back/moeum/src/main/java/com/b301/moeum/user/controller/UserController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:86a12c466f4982c83a586b87ec73dcd2f8eb299c2d2e985e650e77aeade55e7e
-size 6272
+oid sha256:70d7c4e7efdf60e630541046074f7253e5ea80b248599c587e40cc7d28098673
+size 6285
