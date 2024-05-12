60ec41198d889351aeb62114291776cb814f7c7a
주동현 <wn196785@gmail.com>
2024-05-12 22:45:35 +0900

    [feat] confirm page
    Former-commit-id: b9d9fa12b4f3b0fa2eeb386e3132b8f21711d332

--- moeum-front/mediapipe_react/src/MidiComponent.tsx
+++ moeum-front/mediapipe_react/src/MidiComponent.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c2208614a90739a1da7f267d1a25792f73fa5306991486c539a6bef7ebfa25b9
-size 6548
+oid sha256:e75ba0d789fefd1169bf751fb9b3dfcd1a5e53233691f869472726a8e1ad99ed
+size 6542
