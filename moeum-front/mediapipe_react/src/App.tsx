8591e540c7808937a99699c7616b8e754bb71c63
kanggeonnim <gun4329@gmail.com>
2024-05-12 16:11:03 +0900

    refactor: optimize screen render
    Former-commit-id: afdc5261448843ba2d4e838726d20164fde22eb0

--- moeum-front/mediapipe_react/src/App.tsx
+++ moeum-front/mediapipe_react/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:72b8b3f5549774a058e8b7ab931a78aefbb8c085d93dfe72e6813a9048695e59
-size 19595
+oid sha256:e46bd0f130991bd052aa81b204b9be8b47e2449d5403a0c1e840eadda6919a37
+size 19345
