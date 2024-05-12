a8dc16b7f82bb6a231501439db41a61b36cd33e2
주동현 <wn196785@gmail.com>
2024-05-12 16:07:01 +0900

    [feat] download modal add
    Former-commit-id: f8b414456a7cd99331ec9b96a55be7b9a2581866

--- moeum-front/mediapipe_react/src/App.tsx
+++ moeum-front/mediapipe_react/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b83ba65f8f3a081f46e1627ba725f6a1b8f4d3b167d97962264786693cd79441
-size 16930
+oid sha256:ff17433c756dab8e47d38741835addb38610e9b83140afdde947b8df48ef81a6
+size 17279
