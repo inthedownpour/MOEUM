d0d040d2d282074e92bf8c1222cd56cd9513c46f
주동현 <wn196785@gmail.com>
2024-05-12 17:55:09 +0900

    [feat] midi transport
    Former-commit-id: bc4017157b3c220b2359a0db2e9b5cd5e0bd5895

--- moeum-front/mediapipe_react/src/App.tsx
+++ moeum-front/mediapipe_react/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a211808087f25e895d37beb2665506fbf87f63df458c651a59e3e87a09aa9002
-size 17469
+oid sha256:6cc099a541376fac3ae01a86968ca92e75b00341f6c211d192c58d0ba0afa93a
+size 17592
