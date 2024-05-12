3b329237416fd93a8312e60a0614df03bd80cf45
주동현 <wn196785@gmail.com>
2024-05-12 15:45:41 +0900

    [fix] accesstoken added
    Former-commit-id: 95e87512f5143f8cf09c81da41cd15c3d78654f2

--- moeum-front/music_bubble_app/lib/making_music/screen/making_music_screen.dart
+++ moeum-front/music_bubble_app/lib/making_music/screen/making_music_screen.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:313c0096699e484d52d5fd34058b777f95a3948e8d07a492e4a553f8bc7e0c30
-size 6755
+oid sha256:49ce7167d8af8c1d7abf461fb829411523bc36e3d26addd383c40f206f6eea71
+size 7003
