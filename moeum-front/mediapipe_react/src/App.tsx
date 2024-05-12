8615b2d82c6f4c30e7c5cf54561fcd46f6f641e5
주동현 <wn196785@gmail.com>
2024-05-12 20:40:59 +0900

    [fix] structure divided
    Former-commit-id: c6e90f9142778f148a796fef9cf9a41c5c4750ba

--- moeum-front/mediapipe_react/src/App.tsx
+++ moeum-front/mediapipe_react/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6cc099a541376fac3ae01a86968ca92e75b00341f6c211d192c58d0ba0afa93a
-size 17592
+oid sha256:19b9167f2ef86fbcd7b84759af1715742c002ebdd6beb66c74110d79302983da
+size 18915
---
+++ moeum-front/mediapipe_react/src/Record.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1525030a20617839cdcb03d4351dbe1a6c867969ef25c75042e3de814e8c43ae
+size 20468
