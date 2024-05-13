931d5d60938aa75b1952e5da85c4dc22a51e6baf
주동현 <wn196785@gmail.com>
2024-05-13 14:06:25 +0900

    [fix] timeout solve2
    Former-commit-id: 7d41de85f90790c124424dc697ae7e70b8ff51a6

--- moeum-front/mediapipe_react/src/Record.tsx
+++ moeum-front/mediapipe_react/src/Record.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:da99fc8b964eccfad8305b46656f1b40f8b48a00b87eb32e5d2af144be01cc50
-size 20343
+oid sha256:82f049b239ea8fa95def023731285e2e1446fb7bca1fc121ab6701b6525773eb
+size 20542
