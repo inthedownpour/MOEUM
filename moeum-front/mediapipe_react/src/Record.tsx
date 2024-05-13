6a02fdc30d67375b02b30dbf791e16f023089ec7
주동현 <wn196785@gmail.com>
2024-05-14 01:55:22 +0900

    [fix] no parsing
    Former-commit-id: 3ce2b80c1a1826875feb3eb4b02964ef6e804c4f

--- moeum-front/mediapipe_react/src/Record.tsx
+++ moeum-front/mediapipe_react/src/Record.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0d46eafdbce7d3cea60800382fe800f3dbfdff7ee6af7b6f484824bc38464dcf
-size 24812
+oid sha256:ee13a863519994732c9958b7ece82ae51fa3939487d583deefb03c7624d0ad33
+size 24695
