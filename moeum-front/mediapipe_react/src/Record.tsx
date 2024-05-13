f065a35a446dcc4d93db7508218d1a9690b971f7
주동현 <wn196785@gmail.com>
2024-05-13 10:18:00 +0900

    [fix] async update2
    Former-commit-id: 987981c64d5859ad7b7295966bccf23690e0f5c3

--- moeum-front/mediapipe_react/src/Record.tsx
+++ moeum-front/mediapipe_react/src/Record.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d058a2aa6096af648052d1f74e7ef1986cfbf56054a993dd13fb88fed524e5ee
-size 21046
+oid sha256:c84f3b851dc1f54830035a19f19bfa58a8cefd72776d8912766bb8d5f22e25da
+size 22296
