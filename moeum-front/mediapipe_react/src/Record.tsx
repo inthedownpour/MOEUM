4cacc919f06d5cfca425fc1d0611bf51df9dff54
주동현 <wn196785@gmail.com>
2024-05-14 12:56:47 +0900

    [feat] confirm page
    Former-commit-id: cf29a07a6ff72784dc073492297b4878365bf2f1

--- moeum-front/mediapipe_react/src/Record.tsx
+++ moeum-front/mediapipe_react/src/Record.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3c13c0bddd48b1f92bcd49c62a23be60d56fc88be23e2ed397131aeae6f25501
-size 22884
+oid sha256:3192d2bf4a7862a0608fa25245acfb817589fab479bafbcffe1ff9bbaa2007ca
+size 22980
