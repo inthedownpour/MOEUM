7ce8da719d406a68d7adad5ad0cb04f21d8e28dc
주동현 <wn196785@gmail.com>
2024-05-14 21:52:17 +0900

    [fix] vex
    Former-commit-id: 8cf6672e6a990f4bc749385c09aec9ee3209b4f0

--- moeum-front/mediapipe_react/src/Confirm.tsx
+++ moeum-front/mediapipe_react/src/Confirm.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c7f9474a63b1688ca796890d7dca89635ab75134e9c9f6de107a30359cb82259
-size 7269
+oid sha256:ccb8f62f04a2d09f295ac162bb298cb7cc5b366a8b57a2b5b2bbfdf016a24b62
+size 7555
