0027cac835f6fdd9e15fa941b1de08002d286270
주동현 <wn196785@gmail.com>
2024-05-14 09:52:38 +0900

    [fix] 500 solve2
    Former-commit-id: 8fde514eb7d1526a12839aada50c93e9958b6c04

--- moeum-front/music_bubble_app/lib/login/models/kakao_user.dart
+++ moeum-front/music_bubble_app/lib/login/models/kakao_user.dart
@@ -1,3 +1,13 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:787d59eda1c048448b6a34bd3ee8e4a697ffcaf56805f0a9dfba83f633d371da
-size 243
+class KakaoUser {
+  final String id;
+  final String nickname;
+  final String email;
+  final String profileImage;
+
+  KakaoUser({
+    required this.id,
+    required this.nickname,
+    required this.email,
+    required this.profileImage,
+  });
+}
