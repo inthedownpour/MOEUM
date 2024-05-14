c7b6842ddfde4f4655022a3da7bf14eedbc45f0f
주동현 <wn196785@gmail.com>
2024-05-14 21:32:15 +0900

    [fix] confirm3
    Former-commit-id: c5b92ca35eac6f87b85d89582f0fdc1e8c89aa4e

--- moeum-front/mediapipe_react/src/Confirm.tsx
+++ moeum-front/mediapipe_react/src/Confirm.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:50831b0b7e9a74c44a2526a0bdfa7cf66bccaf61bf4d98b955d4faab55cfb9b9
-size 7165
+oid sha256:c7f9474a63b1688ca796890d7dca89635ab75134e9c9f6de107a30359cb82259
+size 7269
