902008e02b615a0efb4995b5c5e767240a582415
kanggeonnim <gun4329@gmail.com>
2024-05-14 11:19:48 +0900

    Refactor: update Record to new version
    Former-commit-id: 933e2ed691ff75949f688ae224a244a8ac3e01ae

--- moeum-front/mediapipe_react/src/Record.tsx
+++ moeum-front/mediapipe_react/src/Record.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3c13c0bddd48b1f92bcd49c62a23be60d56fc88be23e2ed397131aeae6f25501
-size 22884
+oid sha256:af736e93d3255dbcfa49b600fe0ecca1b6fad6ca942a73ec12d5649c73d800b4
+size 16728
