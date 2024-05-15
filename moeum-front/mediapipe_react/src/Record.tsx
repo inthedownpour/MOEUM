4ce6d4ebf1ada8d3f70afa51bdc3b0eb03b91a69
주동현 <wn196785@gmail.com>
2024-05-15 22:09:04 +0900

    [feat] code algo fix2
    Former-commit-id: 1c4853c3251993468272fed74a07c8a7708547a0

--- moeum-front/mediapipe_react/src/Record.tsx
+++ moeum-front/mediapipe_react/src/Record.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d5b1dcd3de69955e9040a61717320a8238d4cb14d4dff2a9a5a3fd2721c86509
-size 25686
+oid sha256:ffb854866112b9f7035222e2b48a9ec02e421b7c34678bc908cd2351cd1b75cc
+size 26335
