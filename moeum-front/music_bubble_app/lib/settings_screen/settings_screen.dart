41b5bd8da659078a5c9a5159060e8e0b8936e216
심지연 <sim.yyeon@gmail.com>
2024-05-16 14:48:51 +0900

    Feat: add Tooltip
    툴팁을 추가하고 툴팁에서 보여줄 메세지를 추가하여 title_widget을 호출하는 부분을 모두 수정
    
    
    Former-commit-id: 960d85f85f57a6ac64df305e5f204cf83f811eb5

--- moeum-front/music_bubble_app/lib/settings_screen/settings_screen.dart
+++ moeum-front/music_bubble_app/lib/settings_screen/settings_screen.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:075c9e0e9b31a77a19b79a873bb646f8d97600e342779305645971431f09d694
-size 8190
+oid sha256:d7fae2bcf56a440973448d1516e25b1993cee4b7fccef229598ace9e8efb2ca4
+size 8337
