adf707c0642f7de32f09fa1d0fa102d90c2ee666
gabin <3to@kakao.com>
2024-05-17 10:36:19 +0900

    refactor: Delete S3 data service
    Former-commit-id: 1f0cb64b8cb46a4716ee2b8064297b92926c9d7e

--- moeum-back/moeum/src/main/java/com/b301/moeum/file/FileRepository.java
+++ moeum-back/moeum/src/main/java/com/b301/moeum/file/FileRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0cf37b4262be2050c493f26c24ff29a241ce1e411b97e8700ab36b2570526f46
-size 347
+oid sha256:7458ac0e33b7c052f97e3e5e7f88e284a008d90b9776fa1f7665f0e06a1bbc90
+size 337
