525986dd3e42ffef0fdf6de19b1e5b996386aa43
주동현 <wn196785@gmail.com>
2024-05-17 20:58:37 +0900

    [fix] flutter fix
    Former-commit-id: 4c5406d424ecc75f0a579050ab189cff746c3c4b

--- moeum-front/mediapipe_react/src/components/Record.tsx
+++ moeum-front/mediapipe_react/src/components/Record.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a19c7b535e4c5ca59504795fbe774f0ddd054eba4a0636ff38b7ce7d0a68bfb8
-size 11528
+oid sha256:febdc2100beafcc9ab78c72984cf455cd03219c36277aebdec388cf2b6d61d20
+size 11660
--- moeum-front/mediapipe_react/src/config/harmony.ts
+++ moeum-front/mediapipe_react/src/config/harmony.ts
@@ -1,3 +1,8 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:538029584aa56d26dfc1f06812f7e49753db9d1a485f5637ea9aa90059ce7d48
-size 162
+const harmony: { [key: string]: string[] } = {
+    'C': ['E', 'B'],
+    'E': ['G', 'B'],
+    'G': ['C', 'B'],
+    'B': ['C', 'G',]
+  };
+
+  export default harmony;
