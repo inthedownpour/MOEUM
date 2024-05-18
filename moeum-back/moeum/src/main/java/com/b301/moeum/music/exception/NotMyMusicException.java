e79f0816c3de25b522644fd30713e2a14af07573
jeongyeon <jeongyeon.park216@gmail.com>
2024-05-18 17:43:12 +0900

    [Feat] Implemented HTML & CSS for arrangement and components for key, bpm, and instruments modification
    Former-commit-id: 70721950ee4aab57c8040c5ff94d50c9b214b322

--- moeum-back/moeum/src/main/java/com/b301/moeum/music/exception/NotMyMusicException.java
+++ moeum-back/moeum/src/main/java/com/b301/moeum/music/exception/NotMyMusicException.java
@@ -1,3 +1,4 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f9d446b9b0e40dfce6c0044b87325d24897364e94f88513bc6e92052064ba8ee
-size 102
+package com.b301.moeum.music.exception;
+
+public class NotMyMusicException extends RuntimeException{
+}
