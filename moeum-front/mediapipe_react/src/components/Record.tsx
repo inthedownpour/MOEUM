dfe7619746a6890753cd80c2864fd806dcf660cb
주동현 <wn196785@gmail.com>
2024-05-18 14:25:39 +0900

    [feat] loading sys add
    Former-commit-id: ac800f711e42cd858ffd534e93434956fa1c6957

--- moeum-front/mediapipe_react/src/components/Record.tsx
+++ moeum-front/mediapipe_react/src/components/Record.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:588acc28a641e13da2dfec7fd80620e00140d69cedad2ac41bb28de0a896a9b8
-size 12201
+oid sha256:c28473d5aea41ac3d636f52bb2cbc2907978acd9c9aaa5d899ad74c223ac55e3
+size 12257
