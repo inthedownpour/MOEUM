336ec19e78c326da83161c4704671195bb9cb8a7
주동현 <wn196785@gmail.com>
2024-05-19 02:48:46 +0900

    [feat] loading gif8
    Former-commit-id: 717c4f40a218f25727aee2dd382c435cbe228a1c

--- moeum-front/mediapipe_react/src/components/Record.tsx
+++ moeum-front/mediapipe_react/src/components/Record.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b4dbbce463d4d5310b8e7160ddca59b573343b33f574eff8ceafee4c348efc57
-size 15303
+oid sha256:60f7878781a1da42bc44f4a885d8f4a72d768c0acecf171cf44cf37ba82f1a6b
+size 13027
