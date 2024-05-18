289edcfa8bcc06391ca164015281b2506e847ae2
주동현 <wn196785@gmail.com>
2024-05-18 16:25:48 +0900

    [fix] loading fix
    Former-commit-id: e2558911a6c6ea66c775d870ac1008470200f2a0

--- moeum-front/mediapipe_react/src/components/Record.tsx
+++ moeum-front/mediapipe_react/src/components/Record.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e3bbec5e34062edbf91899f5faa10cc23089daec2c8c296e206a54cbeaf1e01f
-size 13276
+oid sha256:b828f7ec5bf76f865f994db59fc7bdc30df9b0e435382619564d006ae708610b
+size 13502
