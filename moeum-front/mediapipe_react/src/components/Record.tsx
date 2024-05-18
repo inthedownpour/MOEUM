1b6c13d3facaea07abda3e4db29ea7bc92e2fcda
주동현 <wn196785@gmail.com>
2024-05-19 03:42:11 +0900

    [feat] loading modal
    Former-commit-id: 3e12c84e6d53e19aec11162a2c438edcf3dbf7aa

--- moeum-front/mediapipe_react/src/components/Record.tsx
+++ moeum-front/mediapipe_react/src/components/Record.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:decfdba5924b87f0bd22b2ec83554223124e28fbfe3ee2c03abe00ca3b218a47
-size 12972
+oid sha256:8a210189dd192d2b6aaf0710201a5fedebd06ff9763940596707c2475cfef3a8
+size 13259
