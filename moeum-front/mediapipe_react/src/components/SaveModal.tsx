27768dd2612023eb2b6bdfd8b88bb273486bce91
주동현 <wn196785@gmail.com>
2024-05-19 23:08:45 +0900

    [fix] 403 solve
    Former-commit-id: a56ffba8e241b1fb9b45b85e52f553a40c004f68

--- moeum-front/mediapipe_react/src/components/SaveModal.tsx
+++ moeum-front/mediapipe_react/src/components/SaveModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:82ab134a85394ac9fba4e6d3f7e3a5b8aeb8336375a7c21e0c433baf31bc54bb
-size 6542
+oid sha256:f75a639404dfc1c4416ca5d0d0e17cc77e0019808f4b3e98948efa21d36dc7b4
+size 6657
