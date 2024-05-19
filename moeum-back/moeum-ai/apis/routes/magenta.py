5560fd42b47e45b6cfa1c65622cc86bd991aead4
kanggeonnim <gun4329@gmail.com>
2024-05-19 19:13:50 +0900

    Updated magenta.py to include functionality for editing midi files before generating music based on chords.
    Former-commit-id: 4883d738ece388bb4332cdb4e379ecd63651207a

--- moeum-back/moeum-ai/apis/routes/magenta.py
+++ moeum-back/moeum-ai/apis/routes/magenta.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:21ded2eb99e635fed568219bafd4112f49f38f146fba2dd63f26b8f5dad8eb13
-size 1357
+oid sha256:fecde48b6252b994e95ed97aa68c3502b903da0b0e8095e617eb746fb922f565
+size 1841
