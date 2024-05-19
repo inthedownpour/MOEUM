f86221156d456ae004be8d7981ce9f2aafea1735
이창헌 <changbill@naver.com>
2024-05-19 14:24:58 +0900

    Update README.md
    Former-commit-id: 67b6b34f1a8153ee6964a923f4ce4fe3cbd4ac28

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:044b422ded9a37fe8749ad4d060d8ac3ffafad4e3636ec879867d2b2286a3be0
-size 4545
+oid sha256:127bf168904e00852af88c439adf1487713cb230d1a34705bea8f5fb7cf4b7ea
+size 4687
--- moeum-front/music_bubble_app/lib/settings_screen/widgets/license_widget.dart
+++ moeum-front/music_bubble_app/lib/settings_screen/widgets/license_widget.dart
@@ -1,3 +1,20 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:fd4b5adc2259b3717e644a63c526f8fcfbf72aba6ed47f7ce81a56cecbfef648
-size 429
+import 'package:flutter/material.dart';
+
+Widget licenseWidget(BuildContext context) {
+  return const Column(
+    children: [
+      Center(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Text(
+            "오픈소스 라이센스",
+            style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    ],
+  );
+}
