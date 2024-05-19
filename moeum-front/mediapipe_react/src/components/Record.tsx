dbf25ee5e14c9a481c94330731792d3ea262cb60
kanggeonnim <gun4329@gmail.com>
2024-05-20 01:31:30 +0900

    Refactor: fix Bubble State Bug!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
    Former-commit-id: 2f014a00b9c093d40289466f5d2ce666f9f7c0df

--- moeum-front/mediapipe_react/src/components/Record.tsx
+++ moeum-front/mediapipe_react/src/components/Record.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d552225ebdef40fb8d567c057a9549ffebbbcffd604950708204ec234659a7ae
-size 12028
+oid sha256:3226b923de4a0214737278ed4f4eaec8135711e25c7250af49ca2d398ee32b52
+size 12118
