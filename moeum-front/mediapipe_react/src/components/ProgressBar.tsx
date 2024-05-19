c67ab8c9c080fad30c665712495e918fde337a7c
kanggeonnim <gun4329@gmail.com>
2024-05-19 16:05:06 +0900

    Feature: find error
    Former-commit-id: 4f4533a99060cfb8ebf3139625204df0e9f0c221

--- moeum-front/mediapipe_react/src/components/ProgressBar.tsx
+++ moeum-front/mediapipe_react/src/components/ProgressBar.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1394672276ec5ba3b0fc7bc3d10a7470210ff63ed45e9ec8ea9054133799164a
-size 1658
+oid sha256:98aac8557f6a21a046b09de41b48c64d1e80ab88decb1b03ba729278529dfac4
+size 1838
--- moeum-front/mediapipe_react/src/components/Record.tsx
+++ moeum-front/mediapipe_react/src/components/Record.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4fbae5cc646a44c3d5adefd657b7a2a274faf0b6da8b46cb6881ec12e181d0b7
-size 11964
+oid sha256:d552225ebdef40fb8d567c057a9549ffebbbcffd604950708204ec234659a7ae
+size 12028
