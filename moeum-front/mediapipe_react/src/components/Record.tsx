b98b5aceeda0a32e8ed789ed5af500b6582778ee
주동현 <wn196785@gmail.com>
2024-05-19 23:44:20 +0900

    [fix] 403 solve3
    Former-commit-id: 683e873680603c600e53ede9817ed517f31d9edd

--- moeum-front/mediapipe_react/src/components/Record.tsx
+++ moeum-front/mediapipe_react/src/components/Record.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c9565f8e75a9b087a1da25f8e9b2f96b9c83f6c66b936fa8e092784d98763ce1
-size 13153
+oid sha256:e2e8beb28b83b72b458977ade3956b9a6c59911e4f2da5d63d8a50b4867d73bf
+size 13386
--- moeum-front/mediapipe_react/src/components/SaveModal.tsx
+++ moeum-front/mediapipe_react/src/components/SaveModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7ae660d4df155272b9953dbd4b29d0ab52c23f87873477faafbdcde02d3863a1
-size 6863
+oid sha256:c544184168e845779537f02e57c7d8f0d3cd1ca765d977327e9db9682b89c4dc
+size 6934
