709653c7b113d46cb5d477c75848e47d8644837c
jeongyeon <jeongyeon.park216@gmail.com>
2024-05-20 03:41:25 +0900

    [Fix] Test code 오류 수정:
    Former-commit-id: d44303d7e1b82b37d2a7cccfc120026cce9933ef

--- moeum-back/moeum/src/test/java/com/b301/moeum/music/MusicInfoControllerTest.java
+++ moeum-back/moeum/src/test/java/com/b301/moeum/music/MusicInfoControllerTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ef4b1969c9916a796d1faeb3e2ec6e29dd4fa91049f65ebe058fbc1eef62c9e
-size 18789
+oid sha256:8fa0955bed52f0094b5300c2796e0417a67dec2bf529532c6c736c2f4fd74dad
+size 18648
