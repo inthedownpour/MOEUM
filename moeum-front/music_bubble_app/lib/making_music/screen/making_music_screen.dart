2eae2583f5d9d74cf3ff8b96618988533d55a32e
이창헌 <changbill@naver.com>
2024-05-19 22:41:46 +0900

    [fix] app2web artist name data issue fixed
    Former-commit-id: a9589751bd44fa80c3abadb1480f787dc27a88df

--- moeum-front/music_bubble_app/lib/making_music/screen/making_music_screen.dart
+++ moeum-front/music_bubble_app/lib/making_music/screen/making_music_screen.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9aa13d938f77019bb307c155d35b419b3e05b783d1eba969ad3e6a9de0103eef
-size 8444
+oid sha256:92daaa2f7856d98903ddfe1794ebb5467524c01e4ccd43659410bb6d7e1c38b1
+size 8610
