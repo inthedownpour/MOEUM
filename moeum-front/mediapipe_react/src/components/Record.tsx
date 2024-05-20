b2990a8af365489e6eeb98346971798cc65f9e87
주동현 <wn196785@gmail.com>
2024-05-20 10:26:41 +0900

    [fix] error solve2
    Former-commit-id: 9830ad955510bc2e1447aeb8929c1ec2728b9ad0

--- moeum-front/mediapipe_react/src/components/Record.tsx
+++ moeum-front/mediapipe_react/src/components/Record.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:40152ea4e6c71a0f3e4af4993bfab10afde863944dfe091802ee451ea9826cf6
-size 14556
+oid sha256:01bd1055b8309e803b3f306db5af895d33c94809d3b31cdf391dd4f0d1ea2b0a
+size 14500
