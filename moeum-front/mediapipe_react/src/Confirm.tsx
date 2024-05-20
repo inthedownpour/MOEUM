86ec6724dd8418c667b8be3f4857b4ce77403187
주동현 <wn196785@gmail.com>
2024-05-20 11:27:47 +0900

    [feat] css added
    Former-commit-id: 2e8bf835569736fe2d2ebdb61431da61b20695a4

--- moeum-front/mediapipe_react/src/Confirm.tsx
+++ moeum-front/mediapipe_react/src/Confirm.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ddf637696d75ea5e6fce3e08fca8120b26574d34fcbd33447316a9a3c91978c
-size 7700
+oid sha256:6e93e999a64791ecbf19cacb3db616d4d8ec749cdec49280b2a819a3e1c6c2d8
+size 9578
