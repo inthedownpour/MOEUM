21a6bc75f84914ad79fab5a9ecf100293c64ffd9
주동현 <wn196785@gmail.com>
2024-05-22 15:20:25 +0900

    [fix] self fix2
    Former-commit-id: d4e03d604fa1c740837546063bbb556fc02b7831

--- moeum-front/mediapipe_react/src/components/SaveModal.tsx
+++ moeum-front/mediapipe_react/src/components/SaveModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f1df55723cae9621ab1e80f5115de782098773cc6c26938e3fa992cafc63fe55
-size 12454
+oid sha256:941e4310912aa2f809027831429a31952bae53caa6348c5ee65dd0e3b236e624
+size 12449
